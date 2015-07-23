8d7e390e94c3e637606330e6f9d830ab28f8305e
Paul Adenot <paul@paul.cx>
2015-07-23 12:31:39 +0000

    introducing Mickens' bot

--- index.js
+++ index.js
@@ -12,69 +12,30 @@ function ignore(from) {
     return false;
 }
 
-function getTweet() {
-    return new Promise(function(ok, nope) {
-        request({
-            uri: "http://javascript.horse/random.json"
-        }, function(err, resp, body) {
-            var json;
-            try {
-                json = JSON.parse(body);
-            } catch (err) {
-                nope('Error when retrieving the json feed:'+ err);
-            }
-            ok(json.text);
-        });
-    });
+var quotes = [];
+
+fs = require('fs')
+fs.readFile('mickens.txt', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  quotes = data.split("\n");
+});
+
+function getQuote() {
+  return quotes[Math.floor(Math.random() * quotes.length)];
 }
 
-function repeatPromise(promiseFactory, repeats) {
-    return new Promise(function(ok, nope) {
-        var promises = [];
-        var results = [];
-
-        var promise = promiseFactory();
-        for (var i = 0; i < repeats; i++) {
-            var oldPromise = promise;
-            promise = promiseFactory();
-            oldPromise.then(function(res) {
-                results.push(res);
-                return promise;
-            }, nope);
-        }
-
-        // Final promise
-        promise.then(function(res) {
-            results.push(res);
-            ok(results);
-        }, nope);
-    });
-}
-
-function horsejs(client)
+function mickensbot(client)
 {
     var keywordMap = {};
 
-    const KNOWN_KEYWORDS = ['ember', 'node', 'react', 'angular', 'jquery', 'backbone', 'meteor',
-        'crockford', 'eich', 'rhino', 'spidermonkey', 'v8', 'spartan', 'chakra', 'webkit', 'blink',
-        'jsc'];
+    const KNOWN_KEYWORDS = ['quote'];
 
-    const PRE_LOADED_TWEETS = config.preloadTweets;
     const DEFAULT_CENSORSHIP_PERIOD = config.defaultCensorshipPeriod;
     var censorshipMap = config.censorshipPeriodMap;
 
     var shutupMap = {};
-
-    function maybeCacheTweet(tweet) {
-        for (var j = 0; j < KNOWN_KEYWORDS.length; j++) {
-            var keyword = KNOWN_KEYWORDS[j];
-            if (tweet.toLowerCase().indexOf(keyword) !== -1) {
-                console.log('Found:', keyword, 'in', tweet);
-                keywordMap[keyword] = keywordMap[keyword] || [];
-                keywordMap[keyword].push(tweet);
-            }
-        }
-    }
 
     function setCensorship(chan) {
         var censorshipPeriod = censorshipMap[chan] || DEFAULT_CENSORSHIP_PERIOD;
@@ -84,51 +45,27 @@ function horsejs(client)
         }, censorshipPeriod);
     }
 
-    repeatPromise(getTweet, PRE_LOADED_TWEETS).then(function(tweets) {
-
-        console.log('preloaded tweets', tweets);
-
-        for (var i = 0; i < tweets.length; i++) {
-            maybeCacheTweet(tweets[i]);
-        }
-
         console.log('setting up client');
 
         client.addListener('message', function (from, chan, message) {
             if (ignore(from) || shutupMap[chan])
                 return;
 
-            if (message.indexOf('horsejs') !== -1) {
+            if (message.indexOf('mickensbot') !== -1) {
 
                 if (message.indexOf('shut up') !== -1 || message.indexOf('shutup') !== -1) {
                     setCensorship(chan);
                     return;
                 }
 
-                getTweet().then(function (tweet) {
-                    client.say(chan, tweet);
-                }).catch(function(err) {
-                    client.say(chan, 'Internal error:' + err)
-                });
+	    client.say(chan, getQuote());
                 return;
-            }
-
-            for (var kw in keywordMap) {
-                if (message.toLowerCase().indexOf(kw) !== -1) {
-                    var tweets = keywordMap[kw];
-                    client.say(chan, tweets[Math.random() * tweets.length | 0]);
-                    setCensorship(chan);
-                    break;
-                }
             }
         });
 
-    }, function(err) {
-        console.error('internal error:', err);
-    });
 }
 
-var extensions = [horsejs];
+var extensions = [mickensbot];
 
 function run()
 {
